0b56dc4276404a2ef726c0607cf402f98ca91d7e
celson custodio <celsoncustodio@outlook.com>
2020-04-12 15:22:32 -0300

    Usando Antlr

---
+++ src/main/java/com/mycompany/alguma/lexico/Principal.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.alguma.lexico;
+
+/**
+ *
+ * @author celson
+ */
+import java.io.IOException;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+
+public class Principal {
+     public static void main(String[] args) {
+        try {
+            // args[0] é o primeiro argumento da linha de comando
+            CharStream cs = CharStreams.fromFileName(args[0]);
+            AlgumaLexer lex = new AlgumaLexer(cs);
+            while (lex.nextToken().getType() != Token.EOF) {
+                System.out.println("");
+            }
+        } catch (IOException ex) {
+        }
+    }
+}
